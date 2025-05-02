0a94bb9c0bef2d7b6c87eafee1fb5a48ef41c439
José Ignacio Arellano <jo.arellanor@duocuc.cl>
2025-05-02 15:54:51 -0400

    Agregué SaludoController con endpoint /saludo by JOSE

---
+++ src/main/java/com/equipo/SaludoController.java
@@ -0,0 +1,15 @@
+package com.equipo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SaludoController {
+
+    @GetMapping("/saludo")
+    public String saludar(@RequestParam(defaultValue = "Mundo") String nombre) {
+        return "¡Hola, " + nombre + "!";
+    }
+}
