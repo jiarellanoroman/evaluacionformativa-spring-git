659aa2ce43d5cad253ad8c95c96d4d95b140856b
José Ignacio Arellano <jo.arellanor@duocuc.cl>
2025-05-02 15:25:55 -0400

    primer commit- jose

---
+++ src/main/java/com/equipo/colaborativo/ColaborativoApplication.java
@@ -0,0 +1,13 @@
+package com.equipo.colaborativo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColaborativoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColaborativoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equipo/colaborativo/ColaborativoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipo.colaborativo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColaborativoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
